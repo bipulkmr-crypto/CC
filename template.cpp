bbbfa4c996e40b32ad22ed265b9256920a701b1f
bipulkmr-crypto <bipul1707@gmail.com>
2021-06-14 20:32:05 +0530

    updation

--- template.cpp
+++ template.cpp
@@ -40,73 +40,8 @@ cin.tie(NULL);
 #define fu(i, a, n) for (i = a; i <= n; i++)
 #define fd(i, n, a) for (i = n; i >= a; i--)
 #define gi(n) scanf(% d, &n)
-vector<vector<int>> forest;
-vector<vector<int>> arr;
-vector<bool>visited;
-int pos=0;
-void dfs(int x)
-{
-    visited[x]=true;
-    forest[pos].pb(x);
-    for(int u:arr[x])
-    {   
-        if(!visited[u])
-        {
-            dfs(u);
-        }
-    }
-}
 void still_single()
 {
-    int n,q;
-    cin>>n>>q;
-    forest.resize(n+1);
-    arr.resize(n+1);
-    visited.resize(n+1);
-    int j;
-    int i;
-    rep(i,q)
-    {
-        int a,b;
-        cin>>a>>b;
-        arr[a].pb(b);
-        arr[b].pb(a);
-    }
-    fu(i,1,n)
-    {
-        if(!visited[i])
-        {
-            pos+=1;
-            dfs(i);
-        }
-    }
-    int cnt=0;
-    string ans="";
-    fu(i,1,n)
-    {
-        if(forest[i].size()>0)
-        {
-            cnt++;
-            
-        }
-        else
-        {
-            break;
-        }
-    }
-    cout<<cnt-1<<endl;
-    // cout<<ans<<endl;
-    for(i=1;i<cnt;i++)
-    {
-        if(forest[i].size()>0)
-        {
-            cout<<forest[i][0]<<' '<<forest[i+1][0]<<endl;
-        }
-        else    
-        {
-            break;
-        }
-    }
 }
 /*Don't just sit and hope that God will solve this
 fucking do some	thing, try to observe or solve it a different way.
@@ -115,8 +50,8 @@ If nothing works take a deep breath and start again*/
 int main()
 {
 fast
-int t=1;
-// cin >> t;
+int t;
+cin >> t;
 while (t--)
 {
     still_single();
